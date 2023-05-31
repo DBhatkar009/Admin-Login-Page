137996083ce13b3107d53c373097ca4c3d9b84ee
DBhatkar009 <dhananjay.bhatkar999@outlook.com>
2023-05-31 17:38:38 +0530

    Initial code

---
+++ apps/admin/src/app/nx-welcome.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+/* eslint-disable */
+
+@Component({
+  selector: 'store-nx-welcome',
+  template: `
+  `,
+  styles: [],
+  encapsulation: ViewEncapsulation.None,
+})
+export class NxWelcomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

81316750965cd302729f23ec010dc21f9c324849
Dhananjay Bhatkar <dhananjay.bhatkar999@outlook.com>
2023-06-28 19:29:00 +0530

    Deleted nx-welcome component and fix some changes.

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, MatInputModule],
   providers: [],
   bootstrap: [AppComponent],
 })

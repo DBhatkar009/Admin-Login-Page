f41bf0d18cbe8516c307a9cbd5722690e031b2a4
Dhananjay Bhatkar <dhananjay.bhatkar999@outlook.com>
2023-07-01 00:44:41 +0530

    Fix Error of Router Outlet from app component.

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, MatInputModule],
+  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, ],
   providers: [],
   bootstrap: [AppComponent],
 })

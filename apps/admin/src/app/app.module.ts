564d2acdb237cc60ab6119255bd2e60f556a01b6
Dhananjay Bhatkar <dhananjay.bhatkar999@outlook.com>
2023-08-26 23:17:33 +0530

    Latest changes added new component and style files.

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -3,10 +3,53 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+const UX_MODULE = [FormsModule];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      {
+        path: 'Dashboard',
+        component: DashboardComponent,
+      },
+    ],
+
+  },
+  {
+    path: 'Registration',
+    component: RegistrationComponent
+  },
+
+  {
+    path: 'Login',
+    component: LoginComponent
+  },
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    ShellComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegistrationComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    ...UX_MODULE,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/admin/src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'store-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+})
+export class DashboardComponent implements OnInit {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
+
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method, @typescript-eslint/no-empty-function
+  ngOnInit(): void {}
+}

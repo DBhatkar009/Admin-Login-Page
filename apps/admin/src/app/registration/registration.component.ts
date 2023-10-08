c0d4e69f02767b0bfdce8e6e692a353192206f49
Dhananjay Bhatkar <dhananjay.bhatkar999@outlook.com>
2023-10-08 19:56:15 +0530

    Created Basic Registration form.

--- apps/admin/src/app/registration/registration.component.ts
+++ apps/admin/src/app/registration/registration.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
+  hide = true;
   constructor() {}
 
   ngOnInit(): void {}

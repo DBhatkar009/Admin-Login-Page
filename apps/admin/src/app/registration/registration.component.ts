067ca3284f44b0c1e544185199e40d1bde6050a2
Dhananjay Bhatkar <dhananjay.bhatkar999@outlook.com>
2023-10-08 19:59:55 +0530

    Fix submit button text

--- apps/admin/src/app/registration/registration.component.ts
+++ apps/admin/src/app/registration/registration.component.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,5 +11,6 @@ export class RegistrationComponent implements OnInit {
   hide = true;
   constructor() {}
 
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   ngOnInit(): void {}
 }

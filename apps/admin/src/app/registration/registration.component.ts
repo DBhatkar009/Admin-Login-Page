268dc1416d7e22a2881a3ec2102ac642fc8924b9
Dhananjay Bhatkar <dhananjay.bhatkar999@outlook.com>
2023-10-11 00:22:14 +0530

    Added FormGroup And FormControlName & Validators

--- apps/admin/src/app/registration/registration.component.ts
+++ apps/admin/src/app/registration/registration.component.ts
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'store-registration',
@@ -8,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
-  hide = true;
-  constructor() {}
+  public hide = true;
+  public registrationForm!: FormGroup;
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  ngOnInit(): void {}
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+
+
+  constructor(private fb: FormBuilder) {}
+
+
+  ngOnInit(): void {
+   this.registrationForm = this.fb.group({
+    firstName: new FormControl('',  Validators.required),
+    lastName: new FormControl('',  Validators.required),
+    phoneNumber: new FormControl('', Validators.required),
+    dateOfBirth: new FormControl('', Validators.required),
+    eMailId: new FormControl('', Validators.required),
+    passWord: new FormControl('', Validators.required)
+   });
+  }
+
+
+  public  onSubmit(){
+    console.log(this.registrationForm.controls['firstName'].value);
+    console.log(this.registrationForm.controls['lastName'].value);
+    console.log(this.registrationForm.controls['passWord'].value);
+  }
+
 }
